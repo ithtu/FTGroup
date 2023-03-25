337860639d8cb8c0f7384e4a029e233d76663e51
PhuongTrang27 <124805554+PhuongTrang27@users.noreply.github.com>
2023-03-25 09:26:56 +0700

    Create slide.blade.php

---
+++ views/FontEnd/Layout/slide.blade.php
@@ -0,0 +1,30 @@
+<div class="row carousel-holder">
+    <div class="col-md-12">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <?php $i=0; ?>
+                @foreach($slide as $sl)
+                <li data-target="#carousel-example-generic" data-slide-to="{{$i}}" 
+                @if($i==0) class="active"
+                @endif
+                ></li>
+                <?php $i++;?>
+                @endforeach
+                
+            </ol>
+            <div class="carousel-inner">
+            <?php $i=0; ?>
+            @foreach($slide as $sl)
+                <div @if($i==0) class="item active"
+                @else 
+                class="item"
+                @endif
+                >
+                <?php $i++;?>
+                    <img class="slide-image" src="../resources/images/{{$sl->Hinh}}" alt="">
+                </div>
+                @endforeach
+               
+           
+    </div>
+</div>
