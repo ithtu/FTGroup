522936aad178c3baf7563977c561b4e44c61aa19
PhuongTrang27 <124805554+PhuongTrang27@users.noreply.github.com>
2023-03-25 09:27:15 +0700

    Create footer.blade.php

---
+++ views/FontEnd/Layout/footer.blade.php
@@ -0,0 +1,8 @@
+<hr>
+    <footer>
+        <div class="row">
+            <div class="col-md-12">
+                <p align="center">Trang web &copy; Tin Tức 24h </p>
+            </div>
+        </div>
+    </footer>
