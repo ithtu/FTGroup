6580ce8943f9c6851c5e38afecad7527218f7999
PhuongTrang27 <124805554+PhuongTrang27@users.noreply.github.com>
2023-03-30 12:57:07 +0700

    Delete footer.blade.php

--- views/FontEnd/Layout/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<hr>
-    <footer>
-        <div class="row">
-            <div class="col-md-12">
-                <p align="center">Trang web &copy; Tin Tức 24h </p>
-            </div>
-        </div>
-    </footer>
