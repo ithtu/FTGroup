06c95e92f8339c1ecdf96a5ae5651662597f445f
Tu Nguyen <quoctupdn@gmail.com>
2023-04-06 14:39:19 +0700

    DN-13  Update tim kiem san pham theo ten jira

--- Pages/timkiem.php
+++ Pages/timkiem.php
@@ -72,19 +72,7 @@ session_start();
     <!-- Navbar End -->
 
 
-    <!-- Page Header Start -->
-    <!-- <div class="container-fluid bg-secondary mb-5">
-        <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 300px">
-            <h1 class="font-weight-semi-bold text-uppercase mb-3">DANH MỤC SẢN PHẨM</h1>
-            <div class="d-inline-flex">
-                <p class="m-0"><a href="index.php">Trang chủ</a></p>
-                <p class="m-0 px-2">-</p>
-                <p class="m-0">Danh mục sản phẩm</p>
-            </div>
-        </div>
-    </div> -->
-    <!-- Page Header End -->
-
+   
 
     <!-- Shop Start -->
     <div class="container-fluid pt-5">
