a02ecaf3824cd8bbd5b3e9cdeff10d7e8129c1c7
Tu Nguyen <quoctupdn@gmail.com>
2023-04-06 14:36:51 +0700

    DN-12  Update tim kiem san pham  jira

--- Pages/timkiem.php
+++ Pages/timkiem.php
@@ -16,7 +16,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>PhoneEcommerce</title>
+    <title>T-Phone</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
