afd8a917bbe619bc1bbf4aae22914a3a6f8ba30f
Tu Nguyen <quoctupdn@gmail.com>
2023-04-06 14:41:55 +0700

    DN-14  Update gio hang jira

--- Pages/giohang.php
+++ Pages/giohang.php
@@ -45,19 +45,7 @@
     <!-- Navbar Start -->
     <div class="container-fluid">
         <div class="row border-top px-xl-5">
-            <!-- <div class="col-lg-3 d-none d-lg-block">
-                <a class="btn shadow-none d-flex align-items-center justify-content-between bg-primary text-white w-100" data-toggle="collapse" href="#navbar-vertical" style="height: 65px; margin-top: -1px; padding: 0 30px;">
-                    <h6 class="m-0">Categories</h6>
-                    <i class="fa fa-angle-down text-dark"></i>
-                </a>
-                <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0 bg-light" id="navbar-vertical" style="width: calc(100% - 30px); z-index: 1;">
-                    <div class="navbar-nav w-100 overflow-hidden" style="height: 410px">
-                        <a href="shop.php?quanly=danhmucsanpham$id=1" class="nav-item nav-link">Adidas</a>
-                        <a href="shop.php?quanly=danhmucsanpham$id=2" class="nav-item nav-link">Nike</a>
-                        <a href="shop.php?quanly=danhmucsanpham$id=3" class="nav-item nav-link">Puma</a>
-                    </div>
-                </nav>
-            </div> -->
+            
             <div class="col-lg-9">
                 <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
                     <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
